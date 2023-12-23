0c100e628bb751dbff1bde087b0bc825accd319c
bandara <chathubandara500@gmail.com>
2023-12-24 03:00:53 +0530

    Salary DAO Class And IMPL created

---
+++ src/main/java/lk/ijse/dao/SalaryDaoImpl.java
@@ -0,0 +1,15 @@
+package lk.ijse.dao;
+
+public class SalaryDaoImpl implements SalaryDAO {
+    public void saveSalary(){
+
+    }
+
+    public void updateSalary(){
+
+    }
+
+    public void deleteSalary(){
+
+    }
+}
