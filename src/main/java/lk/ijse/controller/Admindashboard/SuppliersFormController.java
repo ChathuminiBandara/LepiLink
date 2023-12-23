07d1ddabc698c88d707f280f381b41e84e50fd5c
bandara <chathubandara500@gmail.com>
2023-12-24 02:37:23 +0530

    Supplier Form Styled

--- src/main/java/lk/ijse/controller/Admindashboard/SuppliersFormController.java
+++ src/main/java/lk/ijse/controller/Admindashboard/SuppliersFormController.java
@@ -1,8 +1,13 @@
 package lk.ijse.controller.Admindashboard;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import lk.ijse.dto.supplierDto;
+import lk.ijse.model.supplierModel;
+
+import java.sql.SQLException;
 
 public class SuppliersFormController {
     public TableView Suppliers_Table;
@@ -15,12 +20,64 @@ public class SuppliersFormController {
     public TableView payment_Table;
 
     public void SaveOnAction(ActionEvent actionEvent) {
+        saveSupplier();
+    }
+    public void saveSupplier(){
+        String SupplierId = Sup_ID.getText();
+        String SupplierName = Sup_Name.getText();
+        String CompanyName = CompanyNAme.getText();
+        String Description = this.Description.getText();
+        String QtySupplied = this.QtySupplied.getText();
+        String ProductSupplied = this.ProductSupplied.getText();
+
+        var dto = new supplierDto(SupplierId,SupplierName,CompanyName,Description,QtySupplied,ProductSupplied);
+        boolean isSaved = false;
+        try {
+            isSaved = new supplierModel().saveSupplier(dto);
+            if (isSaved){
+                System.out.println("Saved");
+            } else {
+                System.out.println("Not Saved");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void UpdateOnAction(ActionEvent actionEvent) {
+        String id = Sup_ID.getText();
+        String name = Sup_Name.getText();
+        String companyName = CompanyNAme.getText();
+        String description = Description.getText();
+        String qtySupplied = QtySupplied.getText();
+        String productSupplied = ProductSupplied.getText();
+
+        var dto = new supplierDto(id,name,companyName,description,qtySupplied,productSupplied);
+        try {
+            boolean isUpdated = new supplierModel().updateSupplier(dto);
+            if (isUpdated){
+                System.out.println("Updated");
+            } else {
+                System.out.println("Not Updated");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void DeleteOnAction(ActionEvent actionEvent) {
+    public void DeleteOnAction(ActionEvent actionEvent) throws SQLException {
+       String id = Sup_ID.getText();
+       try {
+           boolean isDeleted = new supplierModel().deleteSupplier(id);
+           if (isDeleted) {
+               System.out.println("Deleted");
+           } else {
+               System.out.println("Not Deleted");
+           }
+       }catch (SQLException e){
+           new Alert(Alert.AlertType.WARNING, "Something went wrong!").show();
+       }
+
     }
 
     public void ClearOnAction(ActionEvent actionEvent) {
---
+++ src/main/java/lk/ijse/model/supplierModel.java
@@ -0,0 +1,48 @@
+package lk.ijse.model;
+
+import lk.ijse.db.DbConnection;
+import lk.ijse.dto.supplierDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class supplierModel {
+
+    public static boolean deleteSupplier(String id) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+
+        String sql = "DELETE FROM customer WHERE id = ?";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setString(1, id);
+
+        return pstm.executeUpdate() > 0;
+    }
+
+    public static boolean updateSupplier(supplierDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "UPDATE supplier SET supplierName = ?, supplierCompanyName = ?, dateSupplied = ?, qtySupplied = ? WHERE supplierId = ?";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setString(1, dto.getSupplierName());
+        pstm.setString(2, dto.getSupplierCompanyName());
+        pstm.setString(3, dto.getDateSupplied());
+        pstm.setString(4, dto.getQtySupplied());
+        pstm.setString(5, dto.getSupplierId());
+
+        return  pstm.executeUpdate() > 0;
+    }
+    public  boolean saveSupplier(supplierDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "INSERT INTO supplier VALUES(?,?,?,?,?)";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setString(1, dto.getSupplierId());
+        pstm.setString(2, dto.getSupplierName());
+        pstm.setString(3, dto.getSupplierCompanyName());
+        pstm.setString(4, dto.getDateSupplied());
+        pstm.setString(5, dto.getQtySupplied());
+        boolean isSaved = pstm.executeUpdate() > 0;
+        return isSaved;
+    }
+}
