063e2d6f165493b209ec3beee408f8ec59f3adc8
bandara <chathubandara500@gmail.com>
2023-12-28 21:58:56 +0530

    Created Crud DAO & implemented methods

---
+++ src/main/java/lk/ijse/dao/CrudDAO.java
@@ -0,0 +1,12 @@
+package lk.ijse.dao;
+
+import lk.ijse.dto.paymentDto;
+
+import java.sql.SQLException;
+
+public interface CrudDAO<T> {
+    boolean update(T dto) throws SQLException;
+
+    boolean delete(String id) throws SQLException;
+    boolean save(T dto) throws SQLException;
+}
