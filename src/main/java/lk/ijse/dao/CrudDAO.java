e889cec70852127ebd887e5e94f5790ae953a674
bandara <chathubandara500@gmail.com>
2023-12-28 23:56:08 +0530

    3 BOImpl Created

--- src/main/java/lk/ijse/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dao/CrudDAO.java
@@ -4,7 +4,7 @@ import lk.ijse.dto.paymentDto;
 
 import java.sql.SQLException;
 
-public interface CrudDAO<T> {
+public interface CrudDAO<T> extends SuperDAO {
     boolean update(T dto) throws SQLException;
     boolean delete(String id) throws SQLException;
     boolean save(T dto) throws SQLException;
