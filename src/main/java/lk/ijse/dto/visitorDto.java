d2698576b7910a7eb97e3131aed770c34b269f72
bandara <chathubandara500@gmail.com>
2024-01-09 19:58:29 +0530

    Debugging

--- src/main/java/lk/ijse/dto/visitorDto.java
+++ src/main/java/lk/ijse/dto/visitorDto.java
@@ -9,6 +9,62 @@ import lombok.*;
 @Getter
 
 public class visitorDto {
+    public String getVisitorId() {
+        return visitorId;
+    }
+
+    public void setVisitorId(String visitorId) {
+        this.visitorId = visitorId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public String getPStatus() {
+        return PStatus;
+    }
+
+    public void setPStatus(String PStatus) {
+        this.PStatus = PStatus;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getBId() {
+        return BId;
+    }
+
+    public void setBId(String BId) {
+        this.BId = BId;
+    }
+
     private String visitorId;
     private String Name;
     private String Status;
