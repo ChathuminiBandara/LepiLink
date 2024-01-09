4277e147861377e39124802e520e1dc6506c3762
bandara <chathubandara500@gmail.com>
2024-01-09 19:57:33 +0530

    Debugging

---
+++ src/main/java/lk/ijse/dao/custom/impl/ItemDaoImpl.java
@@ -0,0 +1,6 @@
+package lk.ijse.dao.custom.impl;
+
+import lk.ijse.dao.SuperDAO;
+
+public class ItemDaoImpl implements SuperDAO {
+}
--- src/main/java/lk/ijse/dto/empScheduleDto.java
+++ src/main/java/lk/ijse/dto/empScheduleDto.java
@@ -7,6 +7,62 @@ import lombok.*;
 @ToString
 
 public class empScheduleDto {
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(String supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public String getScheduleLocation() {
+        return scheduleLocation;
+    }
+
+    public void setScheduleLocation(String scheduleLocation) {
+        this.scheduleLocation = scheduleLocation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getScheduleNote() {
+        return scheduleNote;
+    }
+
+    public void setScheduleNote(String scheduleNote) {
+        this.scheduleNote = scheduleNote;
+    }
+
     private String scheduleId;
     private String supervisor;
     private String scheduleLocation;
