dfb3573b2ba422d6a2e6337ff4737d9583a4dfbd
bandara <chathubandara500@gmail.com>
2024-01-09 19:57:25 +0530

    Debugging

--- src/main/java/lk/ijse/dto/employeeDto.java
+++ src/main/java/lk/ijse/dto/employeeDto.java
@@ -9,6 +9,46 @@ import lombok.*;
 public class employeeDto {
 
 
+    public String geteId() {
+        return eId;
+    }
+
+    public void seteId(String eId) {
+        this.eId = eId;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public String getTP() {
+        return TP;
+    }
+
+    public void setTP(String TP) {
+        this.TP = TP;
+    }
+
+    public String getStartedDate() {
+        return startedDate;
+    }
+
+    public void setStartedDate(String startedDate) {
+        this.startedDate = startedDate;
+    }
+
+    public String geteSrt() {
+        return eSrt;
+    }
+
+    public void seteSrt(String eSrt) {
+        this.eSrt = eSrt;
+    }
+
     private String eId;
     private String eName;
     private String TP;
