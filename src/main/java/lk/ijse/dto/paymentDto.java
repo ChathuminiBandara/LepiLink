730f2e47eb267478c9ca31fe18f2c37f28b087ef
bandara <chathubandara500@gmail.com>
2024-01-09 19:58:00 +0530

    Debugging

--- src/main/java/lk/ijse/dto/paymentDto.java
+++ src/main/java/lk/ijse/dto/paymentDto.java
@@ -9,6 +9,39 @@ import lombok.*;
 public class paymentDto {
     private String paymentId;
     private String visitorName;
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getVisitorName() {
+        return visitorName;
+    }
+
+    public void setVisitorName(String visitorName) {
+        this.visitorName = visitorName;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getVisitorContactNum() {
+        return visitorContactNum;
+    }
+
+    public void setVisitorContactNum(String visitorContactNum) {
+        this.visitorContactNum = visitorContactNum;
+    }
+
     private String paymentStatus;
     private String visitorContactNum;
 
