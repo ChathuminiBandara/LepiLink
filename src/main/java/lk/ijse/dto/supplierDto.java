8d40afc7d13ede3c7801925fe9f5316015862e77
bandara <chathubandara500@gmail.com>
2024-01-09 19:58:25 +0530

    Debugging

--- src/main/java/lk/ijse/dto/supplierDto.java
+++ src/main/java/lk/ijse/dto/supplierDto.java
@@ -7,6 +7,54 @@ import lombok.*;
 @NoArgsConstructor
 public class supplierDto {
 
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierCompanyName() {
+        return supplierCompanyName;
+    }
+
+    public void setSupplierCompanyName(String supplierCompanyName) {
+        this.supplierCompanyName = supplierCompanyName;
+    }
+
+    public String getDateSupplied() {
+        return dateSupplied;
+    }
+
+    public void setDateSupplied(String dateSupplied) {
+        this.dateSupplied = dateSupplied;
+    }
+
+    public String getQtySupplied() {
+        return qtySupplied;
+    }
+
+    public void setQtySupplied(String qtySupplied) {
+        this.qtySupplied = qtySupplied;
+    }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
     private String supplierId;
     private String supplierName;
     private String supplierCompanyName;
